928d2f19255cec6d5202335fae6a51cbf7af1224
kalungi256256 <izokal91@gmail.com>
2025-11-29 23:12:34 +0300

    gffee

--- pract.js
+++
@@ -1,8 +0,0 @@
-function updateP(){
-    const pspan = document.getElementById('curentyear');
-    if (pspan){
-        pspan.textContent = new Date().getFullYear();
-    }
-}
-
-document.getElementById('curentyear').textContent = new Date().getFullYear();
